852d444b33daee29460b6f2cf361f5160aefbd54
Krzysztof Czarnecki <czarnecki.krzysiek@gmail.com>
2020-07-26 22:39:50 +0200

    offset and window support in ostd and mibw (extra-resp)

--- extra-resp/exe-mibw.cc
+++ extra-resp/exe-mibw.cc
@@ -2,8 +2,9 @@
 call the program as follows:
 
 ./convert.sh Ada_1A_007Sel_U.txt > _Ada_1A_007Sel_U.txt
-./exe-mibw _Ada_1A_007Sel_U.txt
+./exe-mibw _Ada_1A_007Sel_U.txt 300 120
 
+## ./exe-ostd fname offset(in sec.) window(in sec.)
 */
 
 #include <roj/roj.h>
@@ -12,19 +13,23 @@ call the program as follows:
 using namespace std;
 
 int main(int argc, char **argv){
-  if (argc != 2){
-    cerr << "No input file error!!\n";
+  if (argc != 4){
+    cerr << "Wrong request!! 3 additional args are expected\n";
     throw;
   }
   
   roj_real_array *arr = new roj_real_array (argv[1]);
   roj_complex_signal *sig = new roj_complex_signal (arr);
-  roj_signal_config conf = sig->get_config();
 
   roj_hilbert_equiv *hilbert = new roj_hilbert_equiv(sig);
   roj_complex_signal *xsig = hilbert->get_equivalent();
   roj_complex_signal *icf = xsig -> get_instantaneous_complex_frequency();
 
+  double offset = atof(argv[2]);
+  double window = atof(argv[3]);
+  icf->cut(offset, offset+window);
+  roj_signal_config conf = icf->get_config();
+    
   double mass_ibw = 0.0;
   for (int n=0; n<conf.length; n++){
     double ibw = creal(icf->m_waveform[n]);
@@ -32,8 +37,9 @@ int main(int argc, char **argv){
     else mass_ibw -= ibw;
   }
 
-  cout << "==============================\n";
-  cout << "M-IBW:\t" << mass_ibw / conf.length << " per sample\n";
-    
+  cout << "Final mean:\toffset: "
+       << offset << "\twindow: " << window << "\tresult: "
+       << mass_ibw / conf.length << " rad/s\n";
+
   return 0;
 }
--- extra-resp/exe-ostd.cc
+++ extra-resp/exe-ostd.cc
@@ -2,8 +2,9 @@
 call the program as follows:
 
 ./convert.sh Ada_1A_007Sel_U.txt > _Ada_1A_007Sel_U.txt
-./exe-ostd _Ada_1A_007Sel_U.txt
+./exe-ostd _Ada_1A_007Sel_U.txt 300 120
 
+## ./exe-ostd fname offset(in sec.) window(in sec.)
 */
 
 #include <roj/roj.h>
@@ -12,19 +13,23 @@ call the program as follows:
 using namespace std;
 
 int main(int argc, char **argv){
-  if (argc != 2){
-    cerr << "No input file error!!\n";
+  if (argc != 4){
+    cerr << "Wrong request!! 3 additional args are expected\n";
     throw;
   }
-  
+
   roj_real_array *arr = new roj_real_array (argv[1]);
   roj_complex_signal *sig = new roj_complex_signal (arr);
-  roj_signal_config conf = sig->get_config();
 
   roj_hilbert_equiv *hilbert = new roj_hilbert_equiv(sig);
   roj_complex_signal *xsig = hilbert->get_equivalent();
 
-  roj_array_config ring_conf = {128, -M_PI, M_PI};
+  double offset = atof(argv[2]);
+  double window = atof(argv[3]);
+  xsig->cut(offset, offset+window);
+  roj_signal_config conf = xsig->get_config();
+  
+  roj_array_config ring_conf = {36, -M_PI, M_PI};
   roj_real_array *ring_n = new roj_real_array (ring_conf);
   roj_real_array *ring_v = new roj_real_array (ring_conf);
   roj_real_array *ring_v2 = new roj_real_array (ring_conf);
@@ -46,14 +51,17 @@ int main(int argc, char **argv){
     double ev2 = ring_v2->m_data[n] / ring_n->m_data[n];
     double s = sqrt(ev2 - ev * ev);
     double cv = 100 * s / ev;
-    cout << n << "` " << cv << "%\n";
+    cout << "Part " << (n+1) * 360 / ring_conf.length << ":\toffset: "
+	 << offset << "\twindow: " << window << "\tresult: "
+	 << cv << "%\n";
 
     avg += cv * ring_n->m_data[n];
     mass += ring_n->m_data[n];    
   }
 
-  cout << "==============================\n";
-  cout << "O-STD (normalized):\t" << avg / mass << "%\n";
+  cout << "Final mean:\toffset: "
+       << offset << "\twindow: " << window << "\tresult: "
+       << avg / mass << "%\n";
     
   return 0;
 }
